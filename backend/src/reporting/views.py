e12e6ab5b8eee6616bcca96b7b9e5112b7603efa
Yujith Isura <yujith@Yujiths-MacBook-Pro.local>
2020-04-15 15:59:20 +0530

    frontend work completed for issue #42

--- backend/src/reporting/views.py
+++ backend/src/reporting/views.py
@@ -14,7 +14,7 @@ import os
 
 from .services import get_police_division_summary, get_category_summary, \
     get_mode_summary, get_severity_summary, get_status_summary, get_subcategory_summary, get_district_summary, \
-    get_incident_date_summary, get_slip_data, get_daily_category_data, get_daily_summary_data, get_daily_district_data, get_weekly_closed_complain_category_data, get_weekly_closed_complain_organization_data, get_daily_category_data_with_timefilter
+    get_incident_date_summary, get_slip_data, get_daily_category_data, get_daily_summary_data, get_daily_district_data, get_weekly_closed_complain_category_data, get_weekly_closed_complain_organization_data, get_daily_category_data_with_timefilter, get_organizationwise_data_with_timefilter
 from .functions import apply_style, decode_column_names, incident_type_title, incident_type_query
 
 '''
@@ -64,6 +64,13 @@ class ReportingAccessView(APIView):
             GET parameters => /?template_type=daily_category_with_timefilter&startTime=<startTime>&endTime=<endTime>
             """
             json_dict["file"] = get_daily_category_data_with_timefilter()
+
+        elif (template_type == "organizationwise_total_request_with_timefilter"):
+            """
+            summery_report_organizationwise_with_timefilter
+            GET parameters => /?template_type=organizationwise_total_request_with_timefilter&startTime=<startTime>&endTime=<endTime>
+            """
+            json_dict["file"] = get_organizationwise_data_with_timefilter()
 
         elif (template_type == "weekly_closed_request_category"):
             """
