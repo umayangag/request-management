d3487c1447fa188c47147e0d0ae454df483f477c
umayanga <umayangag@gmail.com>
2020-04-30 16:41:37 +0530

    fix issue #131

--- backend/src/incidents/models.py
+++ backend/src/incidents/models.py
@@ -128,19 +128,6 @@ def generate_complaint_refId(election, district):
     refID = "EC/EDR/%s/%s/%0.4d" % (election, district, current_count+1)
     return refID
 
-def generate_request_refId(category_id):
-    ''' Function to generate refId for requests '''
-    category = Category.objects.get(id=int(category_id))
-    today = datetime.now().replace(hour=0, minute=0, second=0)
-    month = ("0" + str(today.month)) if today.month < 10 else str(today.month)
-    date_info = str(today.day) + month  + str(today.year)[2:]
-    current_count = Incident.objects.filter(
-            incidentType=IncidentType.COMPLAINT,
-            created_date__gte=today
-        ).count()
-    refID = "%s/%s/%0.4d" % (category.code, date_info,current_count+1)
-    return refID
-
 class Incident(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -235,9 +222,7 @@ class Incident(models.Model):
         # if self.incidentType == IncidentType.INQUIRY.name :
         #     self.refId = generate_inquiry_refId(election=self.election, category=self.category, institution=self.institution)
         # else:
-        if(not self.refId): 
-            self.refId = generate_request_refId(self.category)
-            
+
         super(Incident, self).save(*args, **kwargs)
 
     class Meta:
