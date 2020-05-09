b18b5f955b743c67f58f152e29d0d8a406e935fe
YujithIsura <ujithisura@gmail.com>
2020-05-09 12:11:29 +0530

    this fixes #216

--- frontend/src/files/components/FilePicker.js
+++ frontend/src/files/components/FilePicker.js
@@ -3,15 +3,16 @@ import { FilePond, registerPlugin } from "react-filepond";
 import "filepond/dist/filepond.min.css";
 import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
 import LinearProgress from '@material-ui/core/LinearProgress';
-
 import {useLoadingStatus} from '../../loading-spinners/loadingHook'
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+registerPlugin(FilePondPluginFileValidateType);
 
 
 // Register the plugin
 registerPlugin(FilePondPluginFileValidateSize);
 
 export default function FileUploader({ files, setFiles, watchedActions }) {
-    
+  
     const pond = useRef(null);
     const isLoading = useLoadingStatus(watchedActions);
      const FilePondLanguage = {
