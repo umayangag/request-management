308b641c934bec9c272118c43c0589e122704e5d
YujithIsura <ujithisura@gmail.com>
2020-05-12 13:25:14 +0530

    this fixes scrolling issue in home page

--- frontend/src/home/components/Home.js
+++ frontend/src/home/components/Home.js
@@ -24,7 +24,7 @@ const Home = ({classes, ...props}) =>{
             {userCan(user, null, USER_ACTIONS.CAN_REVIEW_ALL_INCIDENTS) && (
                 <>
                 <Grid xs={12} item>
-                    <Card xs={6}>
+                    <Card style={{overflow: "auto"}} xs={6}>
                         <CardHeader
                             title={f({id: "request.management.home.incidents_assigned"})}
                         />
@@ -53,8 +53,8 @@ const Home = ({classes, ...props}) =>{
 
             {!userCan(user, null, USER_ACTIONS.CAN_REVIEW_ALL_INCIDENTS) && (
                 <>
-                <Grid xs={12} item style={{paddingTop:"10px"}}>
-                    <Card xs={6}>
+                <Grid maxWidth={false} xs={12} item style={{paddingTop:"10px",overflow: "auto"}}>
+                    <Card style={{overflow: "auto"}}  xs={6}>
                         <CardHeader
                             title={f({id: "request.management.home.incidents_assigned"})}
                         />
