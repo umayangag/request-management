b52df63f7dfc961a48dc3a1673b92a13b7031593
MrClemRkz <clement.amila@gmail.com>
2020-05-16 22:25:03 +0530

    languge is added to org reports

--- backend/src/reporting/services.py
+++ backend/src/reporting/services.py
@@ -213,12 +213,13 @@ def get_weekly_closed_complain_category_data(language="sinhala"):
 
     return file_dict
 
-def get_organizationwise_data_with_timefilter(start_time, end_time):
+def get_organizationwise_data_with_timefilter(start_time, end_time, language="sinhala"):
 
     file_dict = {}
     file_dict["template"] = "/incidents/complaints/summery_report_organizationwise_with_timefilter.js"
     file_dict["StartDate"] = start_time
     file_dict["EndDate"] = end_time
+    file_dict["language"] = language
 
     incidents = Incident.objects.filter(created_date__range=(parse_date_timezone(start_time), parse_date_timezone(end_time)))
     file_dict["total"] = incidents.count()
@@ -229,11 +230,12 @@ def get_organizationwise_data_with_timefilter(start_time, end_time):
     return file_dict
 
 
-def get_weekly_closed_complain_organization_data():
+def get_weekly_closed_complain_organization_data(language="sinhala"):
 
     file_dict = {}
     file_dict["template"] = "/incidents/complaints/weekly_closed_request_report_organizationwise.js"
     file_dict["date"] = date.today().strftime("%Y/%m/%d")
+    file_dict["language"] = language
 
     week_data = get_weekly_incidents("CLOSED")
     file_dict["StartDate"] = week_data["start_date"]
@@ -247,11 +249,12 @@ def get_weekly_closed_complain_organization_data():
 
     return file_dict
 
-def get_daily_closed_complain_organization_data():
+def get_daily_closed_complain_organization_data(language="sinhala"):
 
     file_dict = {}
     file_dict["template"] = "/incidents/complaints/daily_closed_request_report_organizationwise.js"
     file_dict["date"] = date.today().strftime("%Y/%m/%d")
+    file_dict["language"] = language
 
     start_datetime = (date.today() - timedelta(days=7)).strftime("%Y-%m-%d")
     end_datetime = date.today().strftime("%Y-%m-%d")
